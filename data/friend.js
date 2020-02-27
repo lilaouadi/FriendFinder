20100957eac0cb4676ffeff4f0dd40e621e11fc1
Lila Ouadi <lilaouadi@Lilas-Air.attlocal.net>
2020-02-26 19:39:35 -0800

    working cool

---
+++ data/friend.js
@@ -0,0 +1,56 @@
+var friends = [
+    {
+        "name":"Tina",
+        "photo":"https://images.pexels.com/photos/348520/pexels-photo-348520.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Lila",
+        "photo":"https://images.pexels.com/photos/867462/pexels-photo-867462.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "scores":[
+            1,
+            2,
+            3,
+            4,
+            5,
+            3,
+            2,
+            1,
+            4,
+            5,
+          ]
+      },
+      {
+        "name":"Mohammed",
+        "photo":"https://images.pexels.com/photos/1040626/pexels-photo-1040626.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "scores":[
+            4,
+            3,
+            2,
+            4,
+            5,
+            2,
+            1,
+            3,
+            4,
+            1,
+          ]
+      }
+      
+
+      
+      
+];
+
+module.exports = friends
